f77243180046a75c80d9e87e426b983ff2666721
Mattes D <github@xoft.cz>
2015-03-08 16:26:43 +0100

    Rewritten with better aiming.

--- Main.lua
+++ Main.lua
@@ -20,14 +20,22 @@ end
 
 
 --- Returns the command for srcBot to target dstBot
-local function cmdTargetBot(a_SrcBot, a_DstBot)
+local function cmdTargetBot(a_SrcBot, a_DstBot, a_Game)
+	-- Check params:
 	assert(type(a_SrcBot) == "table")
 	assert(type(a_DstBot) == "table")
+	assert(type(a_Game) == "table")
 	
+	-- Calculate the required angle:
 	local wantAngle = math.atan2(a_DstBot.y - a_SrcBot.y, a_DstBot.x - a_SrcBot.x) * 180 / math.pi
 	local angleDiff = wantAngle - a_SrcBot.angle
+	if (angleDiff < -180) then
+		angleDiff = angleDiff + 360
+	elseif (angleDiff > 180) then
+		angleDiff = angleDiff - 360
+	end
 	
-	-- If the heading is too off, adjust:
+	-- If the current heading is too off, adjust:
 	if (math.abs(angleDiff) > 5) then
 		if ((a_SrcBot.speedLevel > 1) and (math.abs(angleDiff) > 3 * a_SrcBot.maxAngularSpeed)) then
 			-- We're going too fast to steer, brake:
@@ -42,14 +50,11 @@ local function cmdTargetBot(a_SrcBot, a_DstBot)
 		end
 	end
 	
-	-- If the enemy is further than 200 pixels away, accellerate, else brake:
+	-- If the enemy is further than 20 pixels away, accellerate, else nop:
 	local dist = botDistance(a_SrcBot, a_DstBot)
-	if (dist > 40000) then
+	if ((dist > 400) and (a_SrcBot.speed < a_Game.maxBotSpeed)) then
 		aiLog(a_SrcBot.id, "Accellerating (dist is " .. dist .. ")")
 		return { cmd = "accelerate" }
-	elseif (a_SrcBot.speedLevel > 1) then
-		aiLog(a_SrcBot.id, "Braking (dist is " .. dist .. ")")
-		return { cmd = "brake" }
 	else
 		aiLog(a_SrcBot.id, "En route to dst, no command")
 		return nil
@@ -62,13 +67,18 @@ end
 
 --- Converts bot speed to speed level index:
 local function getSpeedLevelIdxFromSpeed(a_Game, a_Speed)
+	-- Try the direct lookup first:
+	local level = a_Game.speedToSpeedLevel[a_Speed]
+	if (level) then
+		return level
+	end
+	
+	-- Direct lookup failed, do a manual lookup:
+	print("speed level lookup failed for speed " .. a_Speed)
 	for idx, level in ipairs(a_Game.speedLevels) do
 		if (a_Speed <= level.linearSpeed) then
-			if (idx == 1) then
-				return 1
-			else
-				return idx - 1
-			end
+			print("Manual speed lookup for speed " .. a_Speed .. " is idx " .. idx .. ", linear speed " .. level.linearSpeed)
+			return idx
 		end
 	end
 	return 1
@@ -79,6 +89,9 @@ end
 
 --- Updates each bot to target the nearest enemy:
 local function updateTargets(a_Game)
+	-- Check params:
+	assert(type(a_Game) == "table")
+	
 	-- Update each bot's settings, based on their speed level:
 	for idx, m in ipairs(a_Game.myBots) do
 		m.speedLevel = getSpeedLevelIdxFromSpeed(a_Game, m.speed)
@@ -102,7 +115,7 @@ local function updateTargets(a_Game)
 		-- Navigate towards the target:
 		if (target) then
 			aiLog(m.id, "Targetting enemy #" .. target.id)
-			a_Game.botCommands[m.id] = cmdTargetBot(m, target)
+			a_Game.botCommands[m.id] = cmdTargetBot(m, target, a_Game)
 		end
 	end
 end
@@ -123,6 +136,23 @@ function onGameStarted(a_Game)
 		end
 	end
 	
+	-- Initialize the speed-to-speedLevel table, find min and max:
+	a_Game.speedToSpeedLevel = {}
+	local minSpeed = a_Game.speedLevels[1].linearSpeed
+	local maxSpeed = minSpeed
+	for idx, level in ipairs(a_Game.speedLevels) do
+		a_Game.speedToSpeedLevel[level.linearSpeed] = idx
+		if (level.linearSpeed < minSpeed) then
+			minSpeed = level.linearSpeed
+		end
+		if (level.linearSpeed > maxSpeed) then
+			maxSpeed = level.linearSpeed
+		end
+	end
+	a_Game.speedToSpeedLevel[0] = 1  -- Special case - bots with zero speed are handled as having the lowest speed
+	a_Game.maxBotSpeed = maxSpeed
+	a_Game.minBotSpeed = minSpeed
+	
 	-- Set each bot's target:
 	updateTargets(a_Game)
 end
@@ -132,8 +162,7 @@ end
 
 
 function onGameUpdate(a_Game)
-	-- Update each bot's target
-	updateTargets(a_Game)
+	-- Nothing needed yet
 end
 
 
@@ -182,6 +211,16 @@ end
 
 function onCommandsSent(a_Game)
 	-- Nothing needed
+end
+
+
+
+
+
+function onSendingCommands(a_Game)
+	-- Update the commands just as they are about to be sent
+	commentLog("Sending commands: server time " .. a_Game.serverTime .. ", local time " .. a_Game.localTime .. ", diff = " .. a_Game.localTime - a_Game.serverTime .. " msec")
+	updateTargets(a_Game)
 end
 
 
